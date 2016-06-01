ccae9262a7157b7f9535d70748f0756447ab0131
Magnus Pedersen <magp@redpill-linpro.com>
2016-06-01 10:16:37 +0200

    auto revert on logs and some more modes

--- init.el
+++ init.el
@@ -107,6 +107,12 @@
 (require 'web-mode)
 (require 'dockerfile-mode)
 
+(add-to-list 'auto-mode-alist '("\\.ftl$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.log$" . auto-revert-tail-mode))
+;; (add-to-list 'auto-mode-alist '("\\.log$" . read-only-mode))
+(add-to-list 'auto-mode-alist '("\\.out$" . auto-revert-tail-mode))
+;; (add-to-list 'auto-mode-alist '("\\.out$" . read-only-mode))
+
 
 
 
@@ -142,3 +148,14 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+;; (require 'erc-join)             ; autojoin support is implemented by erc-join.el
+;; (erc-autojoin-enable)           ; enable channel autojoin support, by default
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
